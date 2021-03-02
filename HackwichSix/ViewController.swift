31918c8c60bf0b275cb9dca38d0a46208f832e2c
Akootchook <77140613+Akootchook@users.noreply.github.com>
2021-03-02 10:13:39 -1000

    Initial Commit

---
+++ HackwichSix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HackwichSix
+//
+//  Created by Berdell Akootchook on 3/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
