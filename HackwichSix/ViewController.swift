7e6e4e658afe48143c8e8163367d40a90891d596
Akootchook <77140613+Akootchook@users.noreply.github.com>
2021-03-07 19:03:37 -0800

    Completed parts 1-10. Added countries to vist

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     var myFriendsArray = ["John", "Bob", "Brian"]
+
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return myFriendsArray.count
+     }
+
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
+         let text = myFriendsArray[indexPath.row]
+         cell.textLabel?.text = text
+         return cell
+     }
+
+
+     override func viewDidLoad() {
+         super.viewDidLoad()
+        
+        //Added countries to visit
+        _ = ["Cambodia", "Iceland", "Holland"]
+        
     }
-
-
 }
-
+}
